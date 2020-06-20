f6333c46071f82fd7f04e4033e94964a6d3e04f8
tomotomo0511 <tomokaru0511@gmail.com>
2020-06-21 00:11:54 +0900

    header_scroll_show

--- script.js
+++ script.js
@@ -2,12 +2,50 @@
 //   alert("jQueryが正常に動作しています！");
 // });
 
-$(function() {
+// $(function() {
+//   $('#header').hide();
+// });
+
+// $(function() {
+//   $(window).scroll(function() {
+//     $('#header').show();
+//   });
+// });
+
+$(function() {   
   $('#header').hide();
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        $('#header').show();
+      } else {
+        $('#header').hide();
+      }
+  });
 });
 
-$(function() {
-  $(window).scroll(function() {
-    $('#header').show();
-  });
+// $(function() {
+//   var topHeader = $('#header');    
+//   topHeader.hide();
+//   //スクロールが100に達したらボタン表示
+//   $(window).scroll(function () {
+//       if ($(this).scrollTop() > 100) {
+//         topHeader.show();
+//       } else {
+//         topHeader.hide();
+//       }
+//   });
+//   //スクロールしてトップ
+//   topHeader.click(function () {
+//       $('body,html').animate({
+//           scrollTop: 0
+//       }, 500);
+//       return false;
+//   });
+// });
+
+$('#header').click(function () {
+  $('body,html').animate({
+      scrollTop: 0
+  }, 500);
+  return false;
 });
