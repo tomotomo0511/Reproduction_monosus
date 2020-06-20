8b811045e3ec67e10836805642023a3671a5e064
tomotomo0511 <tomokaru0511@gmail.com>
2020-06-21 00:55:59 +0900

    script.js_edit

--- script.js
+++ script.js
@@ -1,51 +1,11 @@
-// $(function() {
-//   alert("jQueryが正常に動作しています！");
-// });
-
-// $(function() {
-//   $('#header').hide();
-// });
-
-// $(function() {
-//   $(window).scroll(function() {
-//     $('#header').show();
-//   });
-// });
-
-$(function() {   
-  $('#header').hide();
+$(function() {
+  const header = $('#header');
+  header.hide();
   $(window).scroll(function () {
       if ($(this).scrollTop() > 100) {
-        $('#header').show();
+        header.show();
       } else {
-        $('#header').hide();
+        header.hide();
       }
   });
-});
-
-// $(function() {
-//   var topHeader = $('#header');    
-//   topHeader.hide();
-//   //スクロールが100に達したらボタン表示
-//   $(window).scroll(function () {
-//       if ($(this).scrollTop() > 100) {
-//         topHeader.show();
-//       } else {
-//         topHeader.hide();
-//       }
-//   });
-//   //スクロールしてトップ
-//   topHeader.click(function () {
-//       $('body,html').animate({
-//           scrollTop: 0
-//       }, 500);
-//       return false;
-//   });
-// });
-
-$('#header').click(function () {
-  $('body,html').animate({
-      scrollTop: 0
-  }, 500);
-  return false;
 });
